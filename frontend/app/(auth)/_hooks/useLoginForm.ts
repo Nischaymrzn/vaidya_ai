d8ef4becd278d2ae28dc116362c62d967762ce2b
Nischaymrzn <nischaymaharjann@gmail.com>
2025-12-26 15:22:25 +0545

    feat: dashboard page with routing

--- frontend/app/(auth)/_hooks/useLoginForm.ts
+++ frontend/app/(auth)/_hooks/useLoginForm.ts
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTransition } from "react";
@@ -13,12 +14,15 @@ export function useLoginForm() {
     mode: "onSubmit",
   });
 
+  const router = useRouter();
+
   const [pending, startTransition] = useTransition();
 
   const onSubmit = (values: LoginFormData) => {
     startTransition(async () => {
       await loginAction(values);
       toast.success("Login successfully");
+      router.push("/dashboard");
     });
   };
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Home Page</p>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black gap-4">
+      <p>I am Landing Page</p>
+      <Link href="/login">
+        <Button>Login Page</Button>
+      </Link>
     </div>
   );
 }
