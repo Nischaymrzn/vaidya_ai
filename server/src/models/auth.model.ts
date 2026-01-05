cabf8ec9c6b54ff272943bd18253f6bc13940b12
Nischaymrzn <nischaymaharjann@gmail.com>
2026-01-05 16:34:23 +0545

    feat: added user model

---
+++ server/src/models/auth.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { HydratedDocument } from "mongoose";
+import { UserType } from "../types/user.types";
+
+const userSchema = new mongoose.Schema<UserType>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    number: {
+      type: Number,
+    },
+    password: {
+      type: String,
+      select: false,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    deletedAt: Date,
+  },
+  { timestamps: true }
+);
+
+export type UserDocument = HydratedDocument<UserType>;
+
+export const User = mongoose.model<UserType>("User", userSchema);
