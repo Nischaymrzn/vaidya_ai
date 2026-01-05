47eec03d663ee80f66074e337d04f7814df4be33
Nischaymrzn <nischaymaharjann@gmail.com>
2026-01-05 16:29:12 +0545

    feat: add authentication routes

---
+++ server/src/routes/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { AuthController } from "../controller/auth.controller";
+import { middlewares } from "../middlewares/isAuthenticated";
+
+const authRouter = Router();
+
+const authcontroller = new AuthController();
+
+authRouter.post("/login", authcontroller.loginUser);
+authRouter.post("/register", authcontroller.createUser);
+authRouter.get(
+  "/me",
+  middlewares.isAuthenticated,
+  authcontroller.getCurrentUser
+);
+
+export default authRouter;
