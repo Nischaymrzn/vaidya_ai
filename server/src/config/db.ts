7f12725261a3fa732ca44e02e387b1a9fc05943d
Nischaymrzn <nischaymaharjann@gmail.com>
2026-01-05 16:30:35 +0545

    feat: add database configs(connection)

---
+++ server/src/config/db.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { env } from "./env";
+import { logger } from "../lib/logger";
+
+const connectDB = async () => {
+  try {
+    const mongodbURI = env.MONGODB_URI || "";
+    await mongoose.connect(mongodbURI);
+
+    mongoose.connection.on("error", (err) => {
+      logger.error("Mongodb connection error", err);
+    });
+
+    mongoose.connection.on("disconnected", (err) => {
+      logger.error("Mongodb disconnected", err);
+    });
+    logger.info("Mongodb connected successfully");
+  } catch (error: any) {
+    logger.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
