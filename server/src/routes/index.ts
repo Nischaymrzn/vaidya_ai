36b86ec9037fd4a221a97a83e31a47656a494916
Nischaymrzn <nischaymaharjann@gmail.com>
2026-01-27 20:12:22 +0545

    feat: added admin,user routes with middleware

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.routes";
+import adminRouter from "./admin.routes";
+import userRouter from "./user.route";
 
-const mainRouter = Router()
+const mainRouter = Router();
 
-mainRouter.use("/auth", authRouter)
+mainRouter.use("/auth", authRouter);
+mainRouter.use("/admin", adminRouter);
+mainRouter.use("/users", userRouter);
 
-export default mainRouter
+export default mainRouter;
