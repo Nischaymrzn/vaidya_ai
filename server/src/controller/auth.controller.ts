60201dcf99b5363d460c9c390c8c603b6d87a94f
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-07 10:22:22 +0545

    feat: configure controller for reset password

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -1,4 +1,3 @@
-import z from "zod";
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import ApiError from "../exceptions/apiError";
@@ -6,7 +5,12 @@ import ApiResponse from "../utils/apiResponse";
 import asyncHandler from "../utils/asyncHandler";
 import { UserServices } from "../services/auth.service";
 import responseMessages from "../constants/responseMessages";
-import { CreateUserDTO, loginUserDTO } from "../dtos/user.dto";
+import {
+  CreateUserDTO,
+  loginUserDTO,
+  requestPasswordResetDTO,
+  resetPasswordDTO,
+} from "../dtos/user.dto";
 import errorMessages from "../constants/errorMessages";
 
 const userServices = new UserServices();
@@ -60,12 +64,58 @@ export class AuthController {
       );
     }
 
-    const currentUser = await userServices.getCurrentUser(req.user.id);
+    const userId = req.user?.id ?? (req.user?._id != null ? String(req.user._id) : "");
+    const currentUser = await userServices.getCurrentUser(userId);
     return res.json(
       new ApiResponse(
         StatusCodes.OK,
         responseMessages.USER.RETRIEVED,
         currentUser,
+      ),
+    );
+  });
+
+  sendResetPasswordEmail = asyncHandler(async (req: Request, res: Response) => {
+    const parsedData = requestPasswordResetDTO.safeParse(req.body);
+
+    if (!parsedData.success) {
+      return res.status(400).json({
+        success: false,
+        status: 400,
+        message: parsedData.error.flatten().fieldErrors,
+      });
+    }
+
+    await userServices.sendResetPasswordEmail(parsedData.data.email);
+
+    return res.json(
+      new ApiResponse(
+        StatusCodes.OK,
+        "If the email is registered, a reset link has been sent.",
+        {},
+      ),
+    );
+  });
+
+  resetPassword = asyncHandler(async (req: Request, res: Response) => {
+    const parsedData = resetPasswordDTO.safeParse(req.body);
+
+    if (!parsedData.success) {
+      return res.status(400).json({
+        success: false,
+        status: 400,
+        message: parsedData.error.flatten().fieldErrors,
+      });
+    }
+
+    const token = req.params.token as string;
+    await userServices.resetPassword(token, parsedData.data.newPassword);
+
+    return res.json(
+      new ApiResponse(
+        StatusCodes.OK,
+        "Password has been reset successfully.",
+        {},
       ),
     );
   });
