9566a9a4f7c89a3a1bd0c132c13d5af70b5ef756
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-08 18:15:02 +0545

    fix: refactor profile page layout

--- frontend/app/(protected)/profile/layout.tsx
+++ frontend/app/(protected)/profile/layout.tsx
@@ -1,19 +1,9 @@
-import { getCurrentUser } from "@/lib/dal";
-import { ProtectedHeader } from "../_components/protected-header";
-import { redirect } from "next/dist/client/components/navigation";
-import { Role } from "@/lib/definition";
-
 export default async function ProfileLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-  const user = await getCurrentUser();
-  if (!user) redirect("/login");
-  if (user.role === Role.ADMIN) redirect("/admin/users");
-
-  return <>
-    <ProtectedHeader user={user} />
-    <main className="container mx-auto px-4 py-8 max-w-4xl">{children}</main></>;
+  return (
+    <main className="container mx-auto max-w-4xl px-4 py-8">{children}</main>
+  );
 }
