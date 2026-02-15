29807f58a9212ea36d1ec820f3035faf3f108706
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-15 11:32:31 +0545

    refactor: update upload middleware

--- server/src/middlewares/upload.middleware.ts
+++ server/src/middlewares/upload.middleware.ts
@@ -18,10 +18,37 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
+const docFileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  const isImage = file.mimetype.startsWith("image/");
+  const isPdf = file.mimetype === "application/pdf";
+  if (!isImage && !isPdf) {
+    return cb(new Error("Only image or PDF files are allowed!"));
+  }
+  cb(null, true);
+};
+
+const docUpload = multer({
+  storage: storage,
+  fileFilter: docFileFilter,
+  limits: { fileSize: 50 * 1024 * 1024 },
+});
+
 export const uploads = {
   single: (fieldName: string) => upload.single(fieldName),
   array: (fieldName: string, maxCount: number) =>
     upload.array(fieldName, maxCount),
   fields: (fieldsArray: { name: string; maxCount?: number }[]) =>
     upload.fields(fieldsArray),
+};
+
+export const uploadsDocs = {
+  single: (fieldName: string) => docUpload.single(fieldName),
+  array: (fieldName: string, maxCount: number) =>
+    docUpload.array(fieldName, maxCount),
+  fields: (fieldsArray: { name: string; maxCount?: number }[]) =>
+    docUpload.fields(fieldsArray),
 };
