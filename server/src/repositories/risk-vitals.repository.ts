b9dab064ab44dcfda799bb3d498070fbc6730c31
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-15 11:32:54 +0545

    feat: add risk vitals repository

---
+++ server/src/repositories/risk-vitals.repository.ts
@@ -0,0 +1,22 @@
+﻿import {
+  RiskVitals,
+  RiskVitalsDb,
+  RiskVitalsDocument,
+} from "../models/risk-vitals.model";
+
+export interface IRiskVitalsRepository {
+  createMany(data: RiskVitalsDb[]): Promise<RiskVitalsDocument[]>;
+  getByRisk(riskId: string): Promise<RiskVitalsDb[]>;
+}
+
+export class RiskVitalsRepository implements IRiskVitalsRepository {
+  async createMany(data: RiskVitalsDb[]): Promise<RiskVitalsDocument[]> {
+    if (!data.length) return [];
+    return RiskVitals.insertMany(data);
+  }
+
+  async getByRisk(riskId: string): Promise<RiskVitalsDb[]> {
+    const records = await RiskVitals.find({ riskId }).lean();
+    return records as RiskVitalsDb[];
+  }
+}
