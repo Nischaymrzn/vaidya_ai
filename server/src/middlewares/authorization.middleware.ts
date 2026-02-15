2e94147a728e1220461ceeafc5439ead61c1bd43
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-15 11:32:31 +0545

    refactor: update authorization middleware

--- server/src/middlewares/authorization.middleware.ts
+++ server/src/middlewares/authorization.middleware.ts
@@ -60,4 +60,18 @@ const adminOnlyMiddleware = (req: Request, res: Response, next: NextFunction) =>
   }
 }
 
-export const middlewares = { isAuthenticated, adminOnlyMiddleware }
+const userOnlyMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try{
+      if(req.user && req.user.role !== "admin"){
+          next();
+      }else{
+          throw new ApiError(403, "Forbidden, Users only");
+      }
+  }catch(error: Error | any){
+      return res.status(error.statusCode || 403).json(
+          { success: false, message: error.message || "Forbidden"}
+      );
+  }
+}
+
+export const middlewares = { isAuthenticated, adminOnlyMiddleware, userOnlyMiddleware }
