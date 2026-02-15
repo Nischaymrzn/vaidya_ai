733a33cafd89c62e35502a524e1b0496e536fc95
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-15 11:32:49 +0545

    feat: add medical file model

---
+++ server/src/models/medical-file.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { HydratedDocument } from "mongoose";
+import { MedicalFileType } from "../types/medical-file.types";
+
+export type MedicalFileDb = Omit<MedicalFileType, "userId"> & {
+  userId: mongoose.Types.ObjectId;
+  _id?: mongoose.Types.ObjectId;
+};
+
+const medicalFileSchema = new mongoose.Schema<MedicalFileDb>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    publicId: String,
+    type: String,
+    name: String,
+    size: Number,
+    uploadedAt: {
+      type: Date,
+      default: Date.now,
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export type MedicalFileDocument = HydratedDocument<MedicalFileDb>;
+
+export const MedicalFile = mongoose.model<MedicalFileDb>(
+  "MedicalFile",
+  medicalFileSchema,
+);
