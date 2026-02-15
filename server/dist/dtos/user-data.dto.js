d107e159e498e8315da4522cb1fb265f5fad5f74
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-15 11:49:35 +0545

    fix: fix server big and updated .gitignore

--- server/dist/dtos/user-data.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateUserDataDto = void 0;
-const user_data_types_1 = require("../types/user-data.types");
-exports.UpdateUserDataDto = user_data_types_1.UserDataSchema.omit({
-    userId: true,
-    latestVitals: true,
-}).partial();
--- server/dist/routes/ai-insights.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ai_insights_controller_1 = require("../controller/ai-insights.controller");
-const authorization_middleware_1 = require("../middlewares/authorization.middleware");
-const aiInsightsRouter = (0, express_1.Router)();
-const aiInsightsController = new ai_insights_controller_1.AiInsightsController();
-aiInsightsRouter.post("/", authorization_middleware_1.middlewares.isAuthenticated, authorization_middleware_1.middlewares.userOnlyMiddleware, aiInsightsController.generate);
-exports.default = aiInsightsRouter;
