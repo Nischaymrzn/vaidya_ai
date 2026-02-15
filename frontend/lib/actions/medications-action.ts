05a584e6dff9534d4140a95e79a3640b5b9f858e
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-15 11:32:38 +0545

    feat: add medications actions

---
+++ frontend/lib/actions/medications-action.ts
@@ -0,0 +1,27 @@
+﻿"use server";
+
+import { api } from "../api/axios-instance";
+import { API } from "../api/endpoints";
+import { TMedication } from "../definition";
+
+export type ApiResponse<T = unknown> = {
+  success: boolean;
+  message?: string;
+  data?: T;
+};
+
+export async function getMedications(): Promise<ApiResponse<TMedication[]>> {
+  try {
+    const response = await api.get(API.MEDICATIONS.LIST);
+    return {
+      success: true,
+      data: response.data.data || response.data,
+    };
+  } catch (error: Error | any) {
+    const errorMessage =
+      error?.response?.data?.message ||
+      error.message ||
+      "Failed to fetch medications";
+    return { success: false, message: errorMessage };
+  }
+}
