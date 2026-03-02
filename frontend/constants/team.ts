e1b7e8b9689c2e8c2d28048aa7dce00cfcae92c2
Nischaymrzn <nischaymaharjann@gmail.com>
2026-03-02 11:20:00 +0545

    feat: implement vaidya landing page sections and content wiring

---
+++ frontend/constants/team.ts
@@ -0,0 +1,10 @@
+const team = [
+  {
+    name: "Nischay Maharjan",
+    role: "CEO/ Team lead",
+    imageUrl:
+      "https://res.cloudinary.com/dnqet3vq1/image/upload/v1701591333/Nischaymahrjan_1_ofljlo.jpg",
+  },
+];
+
+export default team;
